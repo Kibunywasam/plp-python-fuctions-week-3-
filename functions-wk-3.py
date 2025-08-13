41e722e0e6b4e2305b82b45fb4b9be7bc503f77c
Samuel Kibunywa <126339033+Kibunywasam@users.noreply.github.com>
2025-08-13 18:19:34 +0300

    uploaded answers for python week 3 assignment

--- functions-wk-3.py
+++ functions-wk-3.py
@@ -8,16 +8,13 @@ def calculate_discount(price, discount_percent):
         return price
 
 # Prompting user for input
-try:
-    original_price = float(input("Enter the original price of the item: "))
-    discount = float(input("Enter the discount percentage: "))
+original_price = float(input("Enter the original price of the item: "))
+discount = float(input("Enter the discount percentage: "))
 
 # Calling function and displaying output
-    final_price = calculate_discount(original_price, discount)
+final_price = calculate_discount(original_price, discount)
 
-    if discount >= 20:
-        print(f"Discount applied. Final price: ksh. {final_price}")
-    else:
-        print(f"No discount applied. Final price: ksh. {final_price}")
-except ValueError:
-    print("Please enter valid numeric values for price and discount.")
+if discount >= 20:
+    print(f"Discount applied. Final price: ksh. {final_price}")
+else:
+    print(f"No discount applied. Final price: ksh. {final_price}")
